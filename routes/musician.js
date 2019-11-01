1e9370d1f939ab2304eccaff4f3bcb4f6e9e9dbe
Rajat Gupta <rajatg@chromeinfotech.com>
2019-11-01 18:13:59 +0530

    added API

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,11 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: ok!");
 });
 
+// healthcheck
+router.get('/chrome', (req, res) => {
+  res.status('200').send("CICD Status: ok!");
+});
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
